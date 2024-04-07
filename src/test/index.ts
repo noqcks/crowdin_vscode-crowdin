71f825131dd07d6a72cc94b66ffbe5320cf7c1a5
noqcks <benji@093b.org>
2024-04-07 02:45:38 +0000

    Upgrade dependencies

--- src/test/index.ts
+++ src/test/index.ts
@@ -9,7 +9,7 @@ export function run(testsRoot: string, cb: (error: any, failures?: number) => vo
     });
     mocha.options.color = true;
 
-    import('glob').then(glob => glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+    import('glob').then(globModule => globModule.default('**/**.test.js', { cwd: testsRoot }, (err, files) => {
         if (err) {
             return cb(err);
         }
